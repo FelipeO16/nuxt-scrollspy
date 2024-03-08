54195b774597b1a6f54cf7038d41d031d20586b3
Felipe <felipe16.12.1998@gmail.com>
2024-03-08 18:00:30 -0300

    feat: draggable

---
+++ draggable/components/Draggable.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="fixed" ref="el" :style="style">
+    <slot></slot>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+import { useDraggable } from "@vueuse/core";
+const el = ref<HTMLElement | null>(null);
+
+const props = defineProps<{
+  px: number;
+  py: number;
+}>();
+
+const { x, y, style } = useDraggable(el, {
+  initialValue: { x: props.px, y: props.py },
+});
+</script>
